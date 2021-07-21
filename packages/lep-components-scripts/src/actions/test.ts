bafbaef6e5c0dbaf9e4dce688c021afd27327fbc
shuaiqidekaige <836133817@qq.com>
2021-07-21 23:20:22 +0800

    fix: 新增test命令行

--- packages/lep-components-scripts/src/actions/test.ts
+++ packages/lep-components-scripts/src/actions/test.ts
@@ -1,3 +1,18 @@
-export default () => {
+import path from "path";
+import { runCLI } from "jest";
+import type {Config} from '@jest/types';
+import jestConfig from '../../config/jest.config'
 
-}
+export default (options: { watch: Boolean }) => {
+  const { watch } = options
+  const cwd = process.cwd()
+  const configPath = path.resolve(cwd, 'jest.config.ts')
+
+  const config = {
+    rootDir: cwd,
+    watch,
+    config: {}
+  } as Config.Argv
+
+  runCLI(config, [cwd])
+};
--- packages/lep-components-scripts/src/index.ts
+++ packages/lep-components-scripts/src/index.ts
@@ -1,26 +1,27 @@
 #!/usr/bin/env node
-import { program } from 'commander'
-import pkg from '../package.json'
+import { program } from "commander";
+import pkg from "../package.json";
 
-import Actions from './actions'
+import Actions from "./actions";
 
-program.version(pkg.version)
+program.version(pkg.version);
 
-program.command('build')
-  .description('build components')
-  .action(Actions.build);
+program.command("build").description("build components").action(Actions.build);
 
-program.command('lint [path]')
-.description('lint codes')
-.action(Actions.lint)
+program
+  .command("lint [path]")
+  .description("lint codes")
+  .action(Actions.lint);
 
-program.command('test')
-.description('validate test components')
-.action(Actions.test)
+program
+  .command("test")
+  .option("-watch", "watch test")
+  .description("validate test components")
+  .action(Actions.test);
 
-program.command('site [order]')
-.description('document for your components')
-.action(Actions.site)
+program
+  .command("site [order]")
+  .description("document for your components")
+  .action(Actions.site);
 
-
-program.parse(process.argv)
+program.parse(process.argv);
