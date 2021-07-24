1f7a3b03b8142e3b78ec3596d96f1fc16480719e
shuaiqidekaige <836133817@qq.com>
2021-07-25 01:35:59 +0800

    fix: 修改prettier配置, 将其移出eslint配置

--- packages/lep-components-scripts/src/actions/lint.ts
+++ packages/lep-components-scripts/src/actions/lint.ts
@@ -1,28 +1,33 @@
-import { ESLint } from 'eslint'
-import { Console } from '../utils'
+import { ESLint } from 'eslint';
+import { Console } from '../utils';
+import baseConfig from '@lep-team/eslint-config';
 
-
-export default async (path: string = './components'): Promise<void> => {
-  const cwd = process.cwd()
+export default async (path = './components'): Promise<void> => {
+  const cwd = process.cwd();
   const eslint = new ESLint({
     cwd,
     extensions: ['js', 'jsx', 'ts', 'tsx'],
     fix: true,
-  })
+    baseConfig
+  });
 
   try {
-    const results: Array<ESLint.LintResult> = await eslint.lintFiles(path)
-    await ESLint.outputFixes(results)
-    const errorFiles: Array<ESLint.LintResult> = results.filter((result: ESLint.LintResult) => {
-      return result.messages.length > 0
-    })
+    const results: Array<ESLint.LintResult> = await eslint.lintFiles(path);
+    await ESLint.outputFixes(results);
+    const errorFiles: Array<ESLint.LintResult> = results.filter(
+      (result: ESLint.LintResult) => {
+        return result.messages.length > 0;
+      }
+    );
     errorFiles.forEach((file: ESLint.LintResult) => {
-      const { filePath, messages } = file
-      Console('')
-      Console(filePath, 2)
-      messages.forEach(msg => Console(`${msg.message}\nline: ${msg.line}, column: ${msg.column}`, 2))
-    })
+      const { filePath, messages } = file;
+      Console('');
+      Console(filePath, 2);
+      messages.forEach((msg) =>
+        Console(`${msg.message}\nline: ${msg.line}, column: ${msg.column}`, 2)
+      );
+    });
   } catch (error) {
-    Console(error.message, 2)
+    Console(error.message, 2);
   }
-}
+};
