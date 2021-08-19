81ebce036e51d4f871f94fb4ba5f205f8067f758
zjtt <zhenjintao@xiaomi.com>
2021-08-19 11:00:52 +0800

    config: stylelint-config add rules

--- packages/lep-stylelint-config/index.js
+++ packages/lep-stylelint-config/index.js
@@ -1,15 +1,16 @@
-"use strict";
+'use strict';
 
 module.exports = {
-	extends: [
-        "stylelint-config-standard",
-        "stylelint-config-rational-order"
-    ],
-    plugins: ["stylelint-order"],
-    "ignoreFiles": [
-        "**/*.js",
-        "**/*.ts"
-    ],
-	rules: {
-	}
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['**/*.js', '**/*.ts'],
+  rules: {
+    // 不要使用已被 autoprefixer支持的浏览器前缀
+    'media-feature-name-no-vendor-prefix': true,
+    'at-rule-no-vendor-prefix': true,
+    'at-rule-empty-line-before': true,
+    'selector-no-vendor-prefix': true,
+    'property-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true
+  }
 };
